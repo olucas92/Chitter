c03a2710998c222e9acbcbed0e28d784ed7a6439
Oliver <o.lucas@live.com>
2015-01-30 16:32:51 +0000

    layout.erb file created with welcome message for users

--- server.rb
+++ server.rb
@@ -42,6 +42,12 @@ class Chitter < Sinatra::Base
     redirect to('/')
   end
 
+  helpers do
+
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
+
 
 end
 
